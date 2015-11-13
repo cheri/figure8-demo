0eb00a6562305c6ddba9673b3fd30815cb39ccb9
Sarah H <smharmon@ucsc.edu>
2015-11-12 17:08:52 -0800

    add example test of coherence

--- FIGURE8Demo.py
+++ FIGURE8Demo.py
@@ -1,43 +1,43 @@
 '''
   This file is part of a simple example of how the FIGURE8 system works.
-	https://github.com/cheri/FIGURE8-demo
+  https://github.com/cheri/FIGURE8-demo
 
   Copyright (c) 2015 Sarah Harmon
-  	This source code is free to use under the GNU General Public License (GPL) with author attribution.
+    This source code is free to use under the GNU General Public License (GPL) with author attribution.
 
   OVERVIEW
-	The complete FIGURE8 system writes creative metaphors and similes by:
-		- Selecting a tenor.
-		- Finding an appropriate vehicle (least semantically similar, with
-			few properties in common).
-		- Generating a sentence by using imaginative recall on fixed, valid
-			grammar constructions.
-		- Ranking its generations to decide which one is its "favorite".
+  The complete FIGURE8 system writes creative metaphors and similes by:
+    - Selecting a tenor.
+    - Finding an appropriate vehicle (least semantically similar, with
+      few properties in common).
+    - Generating a sentence by using imaginative recall on fixed, valid
+      grammar constructions.
+    - Ranking its generations to decide which one is its "favorite".
 
-	Commonly associated properties for words (e.g. "queen" having a common
-		association with "royal") are derived via a JavaScript web scraper on
-		online databases (e.g. http://wordassociations.net/).
+  Commonly associated properties for words (e.g. "queen" having a common
+    association with "royal") are derived via a JavaScript web scraper on
+    online databases (e.g. http://wordassociations.net/).
 
-	Creatively associated words are found with a separate module that learns
-		from input literary texts (stored in input.txt).  For an example of how
-		these words are discovered, refer to FindAssocs.py.
+  Creatively associated words are found with a separate module that learns
+    from input literary texts (stored in input.txt).  For an example of how
+    these words are discovered, refer to FindAssocs.py.
 
   USAGE
-	From the command line, type "python FIGURE8Demo.py"
-  	The demo presented here will:
-  		- Select a tenor from NounMetadata.py.
-  		- Find three potential vehicles for that tenor.
-  		- Generate a fixed simile for each tenor-vehicle pairing.
-  		- Rank its comparison based on a few miscellaneous statistics about the pair.
+  From the command line, type "python FIGURE8Demo.py"
+    The demo presented here will:
+      - Select a tenor from NounMetadata.py.
+      - Find three potential vehicles for that tenor.
+      - Generate a fixed simile for each tenor-vehicle pairing.
+      - Rank its comparison based on a few miscellaneous statistics about the pair.
 
   SAMPLE GENERATION
-  	As an example, this demo may generate
-  		(1) "The music is curtsying like a princess."
-  		(2) "The music is like a shuffling dance."
+    As an example, this demo may generate
+      (1) "The music is curtsying like a princess."
+      (2) "The music is like a shuffling dance."
 
-  	Notably, "music" and "dance" share a higher value of semantic similarity
-  	than "music" and "princess".  The complete FIGURE8 system would likely
-  	favor the former simile and reject the latter.
+    Notably, "music" and "dance" share a higher value of semantic similarity
+    than "music" and "princess".  The complete FIGURE8 system would likely
+    favor the former simile and reject the latter.
 '''
 
 import random
@@ -46,17 +46,18 @@ import en
 import FindAssocs
 from NounMetadata import getObjects
 import difflib
+from TestCoherence import *
 
 all_obj = getObjects()
 
 def find_intersection(item1,item2):
-	return list(set(item1["assoc"]+item1["color"]) & set(item2["assoc"]+item2["color"]))
+  return list(set(item1["assoc"]+item1["color"]) & set(item2["assoc"]+item2["color"]))
 
 def find_num_in_common(item1,item2):
-	return len(find_intersection(item1,item2))
+  return len(find_intersection(item1,item2))
 
 def find_perc_in_common(item1,item2):
-	return 100*len(find_intersection(item1,item2))/ (len(item1["assoc"])+len(item1["color"])+len(item2["assoc"])+len(item2["color"])-len(find_intersection(item1,item2)))
+  return 100*len(find_intersection(item1,item2))/ (len(item1["assoc"])+len(item1["color"])+len(item2["assoc"])+len(item2["color"])-len(find_intersection(item1,item2)))
 
 def get_singular(tenor):
   # Manage exceptions that Nodebox handles poorly.
@@ -73,34 +74,34 @@ def make_simile(tenor, vehicle, adj, verb):
 
   simile = ""
   fixed_forms = [
-			"The "+sing_tenor+" is like " + en.noun.article(adj) + " " + vehicle,
-			"The "+sing_tenor+" is " + adj + ", like " + en.noun.article(vehicle)
-	]
+      "The "+sing_tenor+" is like " + en.noun.article(adj) + " " + vehicle,
+      "The "+sing_tenor+" is " + adj + ", like " + en.noun.article(vehicle)
+  ]
 
-	# Sometimes, the verbs are able to be conjugated by the available NodeBox Linguistics database.
+  # Sometimes, the verbs are able to be conjugated by the available NodeBox Linguistics database.
   try:
-		fixed_forms.extend([
-			"The "+sing_tenor+" is " + en.verb.present_participle(verb) + " like " +  en.noun.article(vehicle),
-			"The "+tenor+" " + en.verb.past(verb,person=1) + " like " + en.noun.article(adj) + " " + vehicle,
-			"The "+tenor+", like " + en.noun.article(adj) + " " + vehicle + ", " +  en.verb.past(verb,person=1)
-		])
-		simile = random.choice(fixed_forms)
+    fixed_forms.extend([
+      "The "+sing_tenor+" is " + en.verb.present_participle(verb) + " like " +  en.noun.article(vehicle),
+      "The "+tenor+" " + en.verb.past(verb,person=1) + " like " + en.noun.article(adj) + " " + vehicle,
+      "The "+tenor+", like " + en.noun.article(adj) + " " + vehicle + ", " +  en.verb.past(verb,person=1)
+    ])
+    simile = random.choice(fixed_forms)
 
   except KeyError, e:
-		print "(KeyError: Verb unavailable for NodeBox conjugation.)\n"
-		simile = random.choice(fixed_forms)
+    print "(KeyError: Verb unavailable for NodeBox conjugation.)\n"
+    simile = random.choice(fixed_forms)
 
   return simile
 
 # Finds a vehicle associated with a tenor.
 def find_vehicle(tenor):
-	a = tenor["assoc"]
-	vehicle = tenor
-	b = []
-	while (set(a).isdisjoint(b) or tenor["name"]==vehicle["name"]):
-		vehicle = random.choice(all_obj)
-		b = vehicle["assoc"]
-	return vehicle
+  a = tenor["assoc"]
+  vehicle = tenor
+  b = []
+  while (set(a).isdisjoint(b) or tenor["name"]==vehicle["name"]):
+    vehicle = random.choice(all_obj)
+    b = vehicle["assoc"]
+  return vehicle
 
 # Returns the semantic similarity between two words.
 def sss(s1, s2, type='concept', corpus='webbase'):
@@ -115,56 +116,60 @@ def sss(s1, s2, type='concept', corpus='webbase'):
 # Returns a value indicating the degree of similarity between two input strings, or an input
 # string and an array of strings.
 def similarity(str1, str2):
-	return difflib.SequenceMatcher(None,str1, str2).ratio()
+  return difflib.SequenceMatcher(None,str1, str2).ratio()
 
 # Prints a simple ranking of the comparison between a tenor and vehicle.
-def rank_comparison(tenor, vehicle, metaphor):
-	# Semantic similarity.
-	sem_sim = sss(tenor["name"], vehicle["name"])
-	str_sim = similarity(tenor["name"], vehicle["name"])
+def rank_comparison(tenor, verb, adj, vehicle, metaphor):
+  # Semantic similarity.
+  sem_sim = sss(tenor["name"], vehicle["name"])
+  str_sim = similarity(tenor["name"], vehicle["name"])
 
-	# Conceptual (Semantic) similarity.
-	print("SemSim("+ tenor["name"] + ", " + vehicle["name"] + "): " + str(sem_sim))
+  # Conceptual (Semantic) similarity.
+  print("SemSim("+ tenor["name"] + ", " + vehicle["name"] + "): " + str(sem_sim))
 
-	# String similarity.
-	print("DiffLibSeqSim("+ tenor["name"] + ", " + vehicle["name"] + "): " + str(str_sim))
+  # String similarity.
+  print("DiffLibSeqSim("+ tenor["name"] + ", " + vehicle["name"] + "): " + str(str_sim))
 
-	# Number of properties in common.
-	print("Properties in Common: " + str(find_intersection(tenor,vehicle)))
-	print("Percentage of Common Properties: " + str(find_perc_in_common(tenor,vehicle)))
-	print("Shares major category? ") + str(tenor["type"]==vehicle["type"])
+  # Number of properties in common.
+  print("Properties in Common: " + str(find_intersection(tenor,vehicle)))
+  print("Percentage of Common Properties: " + str(find_perc_in_common(tenor,vehicle)))
+  print("Shares major category? ") + str(tenor["type"]==vehicle["type"])
+
+  # Number of Google results for the noun-verb and adj-noun pairings.
+  print("Number of Google results for '[" +tenor["name"]+"] ["+verb+ "]': " + str(test_coherence(tenor["name"],verb)))
+  print("Number of Google results for '[" +adj+"] ["+tenor["name"]+ "]': " + str(test_coherence(adj,tenor["name"])))
 
 def main():
-	# Choose a tenor at random from NounMetaData.py.
-	tenor = random.choice(all_obj)
+  # Choose a tenor at random from NounMetaData.py.
+  tenor = random.choice(all_obj)
 
-	print "*********************************"
-	print "THE TENOR IS: " + tenor["name"]
+  print "*********************************"
+  print "THE TENOR IS: " + tenor["name"]
 
-	# Find three vehicles for the tenor.
-	for y in range(0, 3):
-		print "*********************************"
-		vehicle = find_vehicle(tenor)
+  # Find three vehicles for the tenor.
+  for y in range(0, 3):
+    print "*********************************"
+    vehicle = find_vehicle(tenor)
 
-		print "The #" + str(y+1) + " VEHICLE is " + vehicle["name"]
+    print "The #" + str(y+1) + " VEHICLE is " + vehicle["name"]
 
-		# Find associated creative words to construct a simile.
-		adj=random.choice(vehicle["adj"])
-		verb=random.choice(vehicle["verb"])
+    # Find associated creative words to construct a simile.
+    adj=random.choice(vehicle["adj"])
+    verb=random.choice(vehicle["verb"])
 
-		if (verb != "[VERB]" and adj != "[ADJ]"):
-			print "THE VERB IS " + verb
-			print "THE ADJ IS " + adj
+    if (verb != "[VERB]" and adj != "[ADJ]"):
+      print "THE VERB IS " + verb
+      print "THE ADJ IS " + adj
 
-			simile = make_simile(tenor["name"], vehicle["name"], adj, verb)
+      simile = make_simile(tenor["name"], vehicle["name"], adj, verb)
 
-			print("SIMILE: '" + simile + ".'")
+      print("SIMILE: '" + simile + ".'")
 
-			### RANK GENERATION ###
-			print "\nRANKING STATISTICS"
-			rank_comparison(tenor,vehicle,simile)
+      ### RANK GENERATION ###
+      print "\nRANKING STATISTICS"
+      rank_comparison(tenor,verb,adj,vehicle,simile)
 
-	print "*********************************"
+  print "*********************************"
 
 if __name__ == '__main__':
-	main()
+  main()
---
+++ TestCoherence.py
@@ -0,0 +1,65 @@
+'''
+  This file is part of a simple example of how the FIGURE8 system works.
+  https://github.com/cheri/FIGURE8-demo
+
+  Copyright (c) 2015 Sarah Harmon
+    This source code is free to use under the GNU General Public License (GPL) with author attribution.
+
+  FUNCTION
+  This file provides an example of a basic test of coherence.  The main FIGURE8 demo system can call
+  this file to determine whether creative constructions have been used before.
+
+  This example uses Python's urllib module to query the number of Google results that exist for
+  "[NOUN] [VERB]", where [NOUN] and [VERB] are input parameters.
+
+  The system can then reason about the coherence (and novelty) of the paired words based on the
+  number of Google results.
+
+  Example, which can be replicated with the main function:
+    "[TURTLE] [DARKENED]" returns 28 results (in 2015).
+    "[TURTLE] [DREAMED]" returns 858 results (in 2015).
+
+    Thus, our system may reason that turtles are more likely to dream than they are to darken.
+
+  More complex queries may make use of advanced Google-fu (such as the use of the AROUND() operator).
+
+'''
+
+import urllib2
+from urllib2 import Request
+from bs4 import BeautifulSoup
+
+def test_coherence(noun,verb):
+  try:
+    # Form our Google query.
+    url = 'https://www.google.com/search?q="' + noun + "+"+ verb + '"'
+
+    # Provide a header so Google will allow us to use their website to search.
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+
+    # Perform the query.
+    req = urllib2.Request(url, headers = headers)
+    resp = urllib2.urlopen(req)
+    source = resp.read()
+
+    # Use Beautiful Soup (http://www.crummy.com/software/BeautifulSoup/)
+    # to find the result statistics for us.
+    soup = BeautifulSoup(source, "html.parser")
+    stats = soup.find('div',{'id':'resultStats'}).text
+
+    # We only want the number associated with the number of Google results.
+    start = 'About '
+    end = ' results'
+    return int(stats[stats.find(start)+len(start):stats.rfind(end)].replace(',',''))
+
+  except Exception as e:
+    print("Simple coherence test failed: " +  str(e))
+    return -1
+
+def main():
+  print '"turtle darkened" returns ' + str(test_coherence("turtle","darkened")) + ' results.'
+  print '"turtle dreamed" returns ' + str(test_coherence("turtle","dreamed")) + ' results.'
+
+if __name__ == '__main__':
+  main()
